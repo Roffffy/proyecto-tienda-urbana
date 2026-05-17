8d22eae36bc5da6e9e69c4fc7c9c05281a5461d7
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-05-17 11:22:26 -0400

    Ampliacion del GlobalExceptoinHandler

--- usuarios/src/main/java/com/tienda_urbana/usuarios/exception/GlobalExceptionHandler.java
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.tienda_urbana.usuarios.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String,String>> noSuchElemntHandler(NoSuchElementException e){
+        Map<String,String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,String>> illegalArgumentHandler(IllegalArgumentException e){
+        Map<String,String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
     
 }
