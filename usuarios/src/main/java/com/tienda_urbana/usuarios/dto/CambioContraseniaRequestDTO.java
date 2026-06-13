c2d9c12c565fa7989ed5b28e93c15ce58f80d721
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-06-12 22:09:22 -0400

    Documentacion de DTO de cambio de contraseña

--- usuarios/src/main/java/com/tienda_urbana/usuarios/dto/CambioContraseniaRequestDTO.java
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/dto/CambioContraseniaRequestDTO.java
@@ -1,5 +1,6 @@
 package com.tienda_urbana.usuarios.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -9,13 +10,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objeto de transferencia de datos para cambiar de contraseña")
 public class CambioContraseniaRequestDTO {
 
-    @NotBlank(message = "ingrese su antigua contraseña")
+    @NotBlank(message = "Ingrese su antigua contraseña")
     @Size(max = 55, min = 8, message = "La contraseña debe contar con minimo 8 caracteres y no superar los 55")
+    @Schema(description = "En este campo se solicita la contraseña antigua del usuario", example = "contraseñaAntigua.123")
     String contraseñaAntigua;
 
     @NotBlank(message = "Ingrese una nueva contrseña")
     @Size(max = 55, min = 8, message = "La contraseña debe contar con minimo 8 caracteres y no superar los 55")
+    @Schema(description = "En este campo se solicita la contraseña nueva que tendra el usuario", example = "contraseñaNueva.321")
     String contraseñaNueva;
 }
