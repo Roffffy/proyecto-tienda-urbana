94e0b37fead794c017025b20803b0b5123bb85f4
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-06-12 22:05:44 -0400

    Documentacion de DTO de visualizacion de datos de usuario

--- usuarios/src/main/java/com/tienda_urbana/usuarios/dto/VisualizarDatosUsuarioResponseDTO.java
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/dto/VisualizarDatosUsuarioResponseDTO.java
@@ -2,6 +2,7 @@ package com.tienda_urbana.usuarios.dto;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objeto de transferencia para los datos de respuesta del usuario")
 public class VisualizarDatosUsuarioResponseDTO {
 
+    @Schema(description = "Nombre del usuario", example = "Nicolas Saavedra", accessMode = Schema.AccessMode.READ_ONLY)
     private String nombre;
+    @Schema(description = "direccion de correo electronico con la que se registra el usuario", example = "nic.saavedrag@duocuc.cl", accessMode = Schema.AccessMode.READ_ONLY)
     private String email;
+    @Schema(description = "Clave para solicitar una recuperacion de cuenta", example = "123456", accessMode = Schema.AccessMode.READ_ONLY)
     private String claveRecuperacion;
+    @Schema(description = "Fecha en la que el usuario creo su cuenta", example = "01-01-2026", accessMode = Schema.AccessMode.READ_ONLY)
     private LocalDate fechaCreacion;
 }
