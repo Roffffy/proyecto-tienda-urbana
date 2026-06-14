edcbc3d8a10e9fda72ec9be09d0c88c4c9077fe4
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-06-14 09:51:34 -0400

    import innecseario

--- usuarios/src/main/java/com/tienda_urbana/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/controller/UsuarioController.java
@@ -1,8 +1,5 @@
 package com.tienda_urbana.usuarios.controller;
 
-
-import java.util.Map;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
