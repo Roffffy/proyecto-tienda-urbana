2515fb365954a0d1290aa1e1289f001dbd78ef09
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-06-14 09:49:42 -0400

    Adaptacion del service a las nuevas excepciones

--- usuarios/src/main/java/com/tienda_urbana/usuarios/service/UsuarioService.java
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/service/UsuarioService.java
@@ -1,7 +1,6 @@
 package com.tienda_urbana.usuarios.service;
 
 import java.time.LocalDate;
-import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
@@ -10,6 +9,9 @@ import com.tienda_urbana.usuarios.dto.CambioContraseniaRequestDTO;
 import com.tienda_urbana.usuarios.dto.CambioEmailRequestDTO;
 import com.tienda_urbana.usuarios.dto.CreacionUsuarioRequestDTO;
 import com.tienda_urbana.usuarios.dto.VisualizarDatosUsuarioResponseDTO;
+import com.tienda_urbana.usuarios.exception.ContraseñaNoCoincideException;
+import com.tienda_urbana.usuarios.exception.CorreoYaRegistradoException;
+import com.tienda_urbana.usuarios.exception.ElementoNoEncontradoException;
 import com.tienda_urbana.usuarios.model.Usuario;
 import com.tienda_urbana.usuarios.repo.UsuarioRepository;
 
@@ -29,34 +31,34 @@ public class UsuarioService {
 
     public VisualizarDatosUsuarioResponseDTO crearUsuario(CreacionUsuarioRequestDTO dto) {
         if (repo.existsByEmail(dto.getEmail())) {
-            throw new IllegalArgumentException("El correo electronico ya esta registrado");
+            throw new CorreoYaRegistradoException(dto.getEmail());
         }
 
         Usuario usuario = repo.save(new Usuario(null,dto.getNombre(), dto.getEmail(), dto.getContraseña(),
                 dto.getClaveRecuperacion(), "Cliente", LocalDate.now()));
 
-        cliente.crearCarrito(usuario.getId());
+        //cliente.crearCarrito(usuario.getId()); cambiarlo
 
         return mapToDto(usuario);
     }
 
     public VisualizarDatosUsuarioResponseDTO verDatosUsuarioPorId(Long id){
-        return repo.findById(id).map(usuario -> mapToDto(usuario)).orElseThrow(() -> new NoSuchElementException("El usuario con ID: " + id + " no existe."));
+        return repo.findById(id).map(usuario -> mapToDto(usuario)).orElseThrow(() -> new ElementoNoEncontradoException("Usuario", id));
     }
 
     public void cambiarContraseña(Long id, CambioContraseniaRequestDTO dto){
-        Usuario usuario = repo.findById(id).orElseThrow(() -> new NoSuchElementException("El usuario con ID: " + id + " no existe."));  
+        Usuario usuario = repo.findById(id).orElseThrow(() -> new ElementoNoEncontradoException("Usuario", id));
         if (!dto.getContraseñaAntigua().equals(usuario.getContraseña())) {
-            throw new IllegalArgumentException("La contraseña antigua no coincide");
+            throw new ContraseñaNoCoincideException(); //Crear exception 
         }
         usuario.setContraseña(dto.getContraseñaNueva());
         repo.save(usuario);
     }
 
     public VisualizarDatosUsuarioResponseDTO cambiarEmail(Long id, CambioEmailRequestDTO dto){
-        Usuario usuario = repo.findById(id).orElseThrow(() -> new NoSuchElementException("El usuario con ID: " + id + " no existe."));
+        Usuario usuario = repo.findById(id).orElseThrow(() -> new ElementoNoEncontradoException("Usuario", id));
         if (repo.existsByEmail(dto.getNuevoEmail())) {
-            throw new IllegalArgumentException("El correo electronico ya esta registrado.");
+            throw new CorreoYaRegistradoException(dto.getNuevoEmail());
         }
         usuario.setEmail(dto.getNuevoEmail());
         return mapToDto(repo.save(usuario));
