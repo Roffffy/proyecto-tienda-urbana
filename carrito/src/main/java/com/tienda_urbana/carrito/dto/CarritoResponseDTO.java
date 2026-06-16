e6b20ab02e019d5a8a43f170d43888ba1e3b9970
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-06-16 12:32:06 -0400

    Documentacion de dtos

--- carrito/src/main/java/com/tienda_urbana/carrito/dto/CarritoResponseDTO.java
+++ carrito/src/main/java/com/tienda_urbana/carrito/dto/CarritoResponseDTO.java
@@ -2,6 +2,7 @@ package com.tienda_urbana.carrito.dto;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,40 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objeto de transferencia de datos para mostrar la lista de productos que contiene un carrito")
 public class CarritoResponseDTO {
 
-
+    @Schema(description = "Lista de productos pertenecientes a un carrito",
+        example = """
+                {
+                    "items": [
+                        {
+                            "productoId": 1,
+                            "nombre": "Polera básica negra",
+                            "categoria": "Poleras",
+                            "talla": "M",
+                            "precio": 14990,
+                            "cantidad": 10
+                        },
+                        {
+                            "productoId": 2,
+                            "nombre": "Polera estampada rock",
+                            "categoria": "Poleras",
+                            "talla": "L",
+                            "precio": 18990,
+                            "cantidad": 10
+                        },
+                        {
+                            "productoId": 3,
+                            "nombre": "Polera oversize blanca",
+                            "categoria": "Poleras",
+                            "talla": "S",
+                            "precio": 16990,
+                            "cantidad": 10
+                        }
+                    ]
+                }
+                """
+    )
     private List<ProductoDTO> items;
 }
--- carrito/src/main/java/com/tienda_urbana/carrito/dto/ProductoDTO.java
+++ carrito/src/main/java/com/tienda_urbana/carrito/dto/ProductoDTO.java
@@ -1,5 +1,6 @@
 package com.tienda_urbana.carrito.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objeto de transferencia de datos para manipular los datos recibidos por el microservicio de catalogo")
 public class ProductoDTO {
 
+    @Schema(description = "Muestra el ID del producto recibido", example = "2")
     private Long productoId;
+    @Schema(description = "Muestra el nombre del producto recibido", example = "Polera estampada rock")
     private String nombre;
+    @Schema(description = "Muestra el nombre de la categoria a la que pertenece el producto recibido", example = "Poleras")
     private String categoria;
+    @Schema(description = "Muestra la talla del producto recibido", example = "XXL")
     private String talla;
+    @Schema(description = "Muestra el precio que tiene el producto recibido", example = "18990")
     private int precio;
+    @Schema(description = "Este campo si lo manipula el microservicio de carrito al momento de agregar un producto para determinar la cantidad solicitada", example = "3")
     private int cantidad;
 
 }
