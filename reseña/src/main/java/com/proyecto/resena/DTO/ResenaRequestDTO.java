b62a270d6f65dc6da1a2be027aefc9db7bcfb870
Nicolas Saavedra <nic.saavedrag@duocuc.cl>
2026-06-17 17:15:43 -0400

    documentacion hecha sobre reseña

--- reseña/src/main/java/com/proyecto/resena/DTO/ResenaRequestDTO.java
+++ reseña/src/main/java/com/proyecto/resena/DTO/ResenaRequestDTO.java
@@ -1,5 +1,6 @@
 package com.proyecto.resena.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -13,15 +14,32 @@ public class ResenaRequestDTO {
     @NotNull(message = "es obligatorio colocar su clasificaciones si quieres darle una reseña")
     @Min(value = 1, message = "la clasificacion minima es de 1 estrella")
     @Max(value = 5, message = "la clasificacion maxima es de 5 estrellas")
+    @Schema(
+        description="clasificacion de la reseña otorgada por el usuario (de 1 a 5 estrellas)",
+        example="5",
+        requiredMode=Schema.RequiredMode.REQUIRED
+    )
     private Integer clasificacion;
 
     @Size(min = 2, max = 400, message = "la cantidad de caracteres deb ser entre 2 y 400")
     @NotBlank(message = "el comentario es obligatorio")
+    @Schema(
+    description = "comentario de la reseña",
+    example = "excelente producto, lo recomiendo",
+    requiredMode = Schema.RequiredMode.REQUIRED)
     private String comentario;  
 
     @NotNull(message = "el usuario es obligaotrio")
+    @Schema(
+    description = "ID del usuario que realiza la reseña",
+    example = "12",
+    requiredMode = Schema.RequiredMode.REQUIRED)
     private Long usuarioId;
 
     @NotNull(message = "el producto es obligatorio")
+    @Schema(
+    description = "ID del producto reseñado",
+    example = "45",
+    requiredMode = Schema.RequiredMode.REQUIRED)
     private Long productoId;
 }
