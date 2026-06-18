1befadb7f05d2a86b1a4507759045cf066f61d0f
Nicolas Saavedra <nic.saavedrag@duocuc.cl>
2026-06-18 00:36:36 -0400

    envio avanzado

--- envio/src/main/java/com/proyecto/envio/DTO/EnvioResponseDTO.java
+++ envio/src/main/java/com/proyecto/envio/DTO/EnvioResponseDTO.java
@@ -2,7 +2,6 @@ package com.proyecto.envio.DTO;
 
 import java.time.LocalDateTime;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EnvioResponseDTO {
-    @Schema(
-        description="ID único del envio, generado automáticamente",
-        example="1",
-        accessMode=Schema.AccessMode.READ_ONLY
-    )
     private Long id;
 
     private String direccion;
