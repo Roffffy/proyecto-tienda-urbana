4b5ba0701fd5cf4a06d4b37f3e9567e86f01447e
Nicolas Saavedra <nic.saavedrag@duocuc.cl>
2026-07-05 00:29:54 -0400

    devolucion hecha

--- devolucion/src/main/java/com/proyecto/devolucion/Client/OrdenesClient.java
+++ devolucion/src/main/java/com/proyecto/devolucion/Client/OrdenesClient.java
@@ -4,10 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "usuarios")
-public interface DevolucionClient {
+@FeignClient(name = "ms.ordenes")
+public interface OrdenesClient {
 
-    @PostMapping("/api/devoluciones/{id}")
-    void crearUsuario(@PathVariable Long id);
-
+    @PostMapping("/api/ordenes/{id}")
+    void crearOrden(@PathVariable Long id);
 }
